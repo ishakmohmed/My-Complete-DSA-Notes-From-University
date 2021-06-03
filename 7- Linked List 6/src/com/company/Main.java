8f29323d91895b3496da995aba8bbd5c6f396c59
Mohmed Ishak <ishakmohmed1911@gmail.com>
2021-06-03 12:43:47 +0800

    Added Linked List notes.

---
+++ 7- Linked List 6/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("car1");
+        cars.add("car2");
+
+        Iterator<String> it = cars.iterator();
+        System.out.println(it.next());
+
+        while (it.hasNext())
+            System.out.println(it.next());
+    }
+}
