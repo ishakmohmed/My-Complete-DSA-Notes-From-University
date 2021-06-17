ce8ac4f5df81b4ea2de1157b6f3bb09ba93827ba
Mohmed Ishak <ishakmohmed1911@gmail.com>
2021-06-17 13:28:13 +0800

    Added more notes on stack.

---
+++ 13- Stack 2/src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Stack<String> stackOfCards = new Stack<>();
+
+	    stackOfCards.push("Jack");
+	    stackOfCards.push("Queen");
+	    stackOfCards.push("King");
+	    stackOfCards.push("Ace");
+
+        System.out.println("Stack => " + stackOfCards);
+        System.out.println();
+
+        String cardAtTop = stackOfCards.pop();
+        System.out.println("Stack.pop() => " + cardAtTop);
+        System.out.println("Current Stack => " + cardAtTop);
+        System.out.println();
+
+        cardAtTop = stackOfCards.peek();
+        System.out.println("Stack.peek() => " + cardAtTop);
+        System.out.println("Current Stack => " + stackOfCards);
+    }
+}
---
+++ 14- Stack 3/src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Stack<String> stackOfCards = new Stack<>();
+        stackOfCards.push("Jack");
+        stackOfCards.push("Queen");
+        stackOfCards.push("King");
+        stackOfCards.push("Ace");
+
+        System.out.println("Stack : " + stackOfCards);
+
+        System.out.println("Is Stack empty ? : " + stackOfCards.isEmpty());
+
+        System.out.println("Size of Stack : " + stackOfCards.size());
+
+        int position = stackOfCards.search("Queen");
+
+        if (position != -1) {
+            System.out.println("Found the element \"Queen\" at position : " + position);
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+}
