bccc034c62da8ab76069793f6ff9707c0c94d81e
Mohmed Ishak <ishakmohmed1911@gmail.com>
2021-06-24 12:58:22 +0800

    Update Main.java

--- 7- Linked List 6/src/com/company/Main.java
+++ 7- Linked List 6/src/com/company/Main.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Main {
-
     public static void main(String[] args) {
         ArrayList<String> cars = new ArrayList<String>();
         cars.add("car1");
